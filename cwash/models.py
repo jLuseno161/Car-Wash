74179419413ccdc6bac416e56510b88c6e7389d7
jLuseno161 <joyluseno2@gmail.com>
2021-07-27 08:31:38 +0300

    profile model  class

--- cwash/models.py
+++ cwash/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.IntegerField(default=0)
+    email = models.CharField(max_length=30, blank=True)
+    profile_pic = CloudinaryField('profile')
+    bio = models.TextField(max_length=500, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Profile.objects.get(user_id=id)
+
+
