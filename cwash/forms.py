58816c78cdd497b5d5a9f91bbef0c27a6935ebe3
jLuseno161 <joyluseno2@gmail.com>
2021-07-27 08:29:32 +0300

    signup & profile forms

---
+++ cwash/forms.py
@@ -0,0 +1,25 @@
+from cwash.models import Profile
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['email','profile_pic', 'bio','neighbourhood']
+
