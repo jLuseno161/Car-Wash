6d4a39b891ca190dd769d1c839b9c57c8250b92b
jLuseno161 <joyluseno2@gmail.com>
2021-07-27 08:45:33 +0300

    update view functions

--- cwash/views.py
+++ cwash/views.py
@@ -1,3 +1,55 @@
+from cwash.models import Profile
+from django.contrib.auth.models import User
+from cwash.forms import SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    
+     return HttpResponse('Hi there')
+
+
+
+def signup(request):
+    print('here')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    # posts = Post.objects.filter(user=current_user.id).all
+    return render(request, 'registration/profile.html')
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request, id):
+    obj = get_object_or_404(Profile, user_id=id)
+    obj2 = get_object_or_404(User, id=id)
+    form = UpdateProfileForm(request.POST or None, request.FILES, instance=obj)
+    form2 = UpdateUserForm(request.POST or None, instance=obj2)
+    if form.is_valid() and form2.is_valid():
+        form.save()
+        form2.save()
+        return HttpResponseRedirect("/profile")
+
+    return render(request, "registration/update_profile.html", {"form": form, "form2": form2})
