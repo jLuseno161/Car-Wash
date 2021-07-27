61f9a2589018790b6bf0ad60d0611bebc2210efb
jLuseno161 <joyluseno2@gmail.com>
2021-07-27 23:46:42 +0300

    update migrations

--- cwash/admin.py
+++ cwash/admin.py
@@ -1,4 +1,12 @@
-from cwash.models import Profile
+from cwash.models import Booking, Profile, Washplan, services
 from django.contrib import admin
 
+class WashplanAdmin(admin.ModelAdmin):
+    filter_horizontal =('service',)
+
 admin.site.register(Profile)
+admin.site.register(Booking)
+admin.site.register(Washplan,WashplanAdmin)
+admin.site.register(services)
+
+
--- cwash/models.py
+++ cwash/models.py
@@ -4,13 +4,14 @@ from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     contact = models.IntegerField(default=0)
     email = models.CharField(max_length=30, blank=True)
     profile_pic = CloudinaryField('profile')
     bio = models.TextField(max_length=500, blank=True)
-    
+
     def __str__(self):
         return self.user.username
 
@@ -32,33 +33,47 @@ class Profile(models.Model):
 
 class Booking(models.Model):
     user = user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.CharField(max_length=30, blank=True)
-    mobile = models.IntegerField(default=0)
+    email = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='emails')
+    mobile = models.IntegerField()
     appointment_date = models.DateTimeField(auto_now_add=True)
-    timeframe =(
-        ('Morning', 'Morning'),
-        ('Evening', 'evening'),
-        ('Afternoon', 'afternoon'),)
-    
+    time_frame = (
+        ("Morning", 'Morning'),
+        ("Evening", 'Evening'),
+        ("Afternoon", 'Afternoon'))
+    timeframe = models.CharField(
+        choices=time_frame, default=0, blank=False, max_length=50)
+
+
 # class Washplan(models.Model):
 #     basic = models.ForeignKey(Basic, on_delete)
-#     deluxe = 
-#     ultimate = 
-#     premium = 
+#     deluxe =
+#     ultimate =
+#     premium =
+class services(models.Model):
+    service = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.service
+
 
 class Washplan(models.Model):
     plan = models.CharField(max_length=50)
-    price = models.IntegerField(max_length=50)
+    price = models.IntegerField()
     duration = models.CharField(max_length=50)
-    hwash= models.CharField(max_length=50)
-    hdry = models.CharField(max_length=50)
-    wshine = models.CharField(max_length=50)
-    tdress = models.CharField(max_length=50)
-    window = models.CharField(max_length=50)
-    sealer = models.CharField(max_length=50)
-    vacuum = models.CharField(max_length=50)
-    dashboard = models.CharField(max_length=50)
-    airfreshner = models.CharField(max_length=50)
-    coat = models.CharField(max_length=50)
-    vinyl = models.CharField(max_length=50)
-    rainx = models.CharField(max_length=50)
+    service = models.ManyToManyField(services)
+
+    def __str__(self):
+        return self.plan
+    # hwash = models.CharField(max_length=50, blank=True, null=True)
+    # hdry = models.CharField(max_length=50, blank=True, null=True)
+    # wshine = models.CharField(max_length=50, blank=True, null=True)
+    # tdress = models.CharField(max_length=50, blank=True, null=True)
+    # window = models.CharField(max_length=50, blank=True, null=True)
+    # sealer = models.CharField(max_length=50, blank=True, null=True)
+    # vacuum = models.CharField(max_length=50, blank=True, null=True)
+    # dashboard = models.CharField(max_length=50, blank=True, null=True)
+    # airfreshner = models.CharField(max_length=50, blank=True, null=True)
+    # coat = models.CharField(max_length=50, blank=True, null=True)
+    # vinyl = models.CharField(max_length=50, blank=True, null=True)
+    # rainx = models.CharField(max_length=50, blank=True, null=True)
